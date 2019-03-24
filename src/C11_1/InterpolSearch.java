facfa8cf18f1269d4f858fa09ee64facecf53580
narshaKim <narsha9501@gmail.com>
2019-03-24 15:56:30 +0900

    10. 정렬(Sorting) #9 10-2. 복잡하지만 효율적인 정렬 알고리즘 보간탐색 보완

--- src/C11_1/InterpolSearch.java
+++ src/C11_1/InterpolSearch.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InterpolSearch {
 
     public static int recursiveBinarySearch(int k, int[] arr, int start, int end) {
-        if(start>end)
+        if(k<arr[start] || k>arr[end])
             return -1;
         int mid = (int)((double)(k-arr[start])/(arr[end]-arr[start]))*(end-start)+start;
         if(arr[mid]<k)
@@ -17,15 +17,19 @@ public class InterpolSearch {
     }
 
     public static void main(String[] args) {
+        int test = 100;
         int range = 10;
         int arrayLength = 10;
-        int[] arr = new int[arrayLength];
-        for(int i=0; i<arrayLength; i++) {
-            arr[i]=(int) Math.floor(Math.random()*range);
+
+        for(int t=0 ; t<test; t++) {
+            int[] arr = new int[arrayLength];
+            for(int i=0; i<arrayLength; i++) {
+                arr[i]=(int) Math.floor(Math.random()*range);
+            }
+            Arrays.sort(arr);
+            int idx = recursiveBinarySearch(2, arr, 1, arr.length-1);
+            System.out.println(idx);
         }
-        Arrays.sort(arr);
-        int idx = recursiveBinarySearch(3, arr, 0, arrayLength-1);
-        System.out.println(idx);
     }
 
 }
